9b29f8687121230526464edc036701faa0f392f8
Gagik <gagikpog@gmail.com>
2018-02-22 16:17:41 +0500

    Added food and a collision with ghosts

--- PackMan/Header.h
+++ PackMan/Header.h
@@ -7,12 +7,17 @@
 
 enum Cubes
 {
-	C_void,C_wall,C_track
+	C_void,C_wall,C_track,C_drug,C_food
 };
 
 enum M_direct
 {
 	d_up,d_left,d_down,d_right
+};
+
+enum Game_stat
+{
+	G_game, G_pause, G_win,G_over
 };
 
 M_direct& operator!(M_direct& f)
@@ -31,7 +36,7 @@ class Man
 public:
 	Man(std::vector<std::vector<Cubes>>* matrix) 
 	{
-	Matrix = matrix;
+		Matrix = matrix;
 	}
 	~Man() {}
 	int x = 3, y = 5;
@@ -41,31 +46,31 @@ public:
 		switch (nextdir)
 		{
 		case d_right:
-			if ((x == (*Matrix)[0].size() - 1) && ((*Matrix)[y][0] == C_track))
+			if ((x == (*Matrix)[0].size() - 1) && ((*Matrix)[y][0] == C_track || (*Matrix)[y][0] == C_food))
 				x = 0;
 			if (x < (*Matrix)[0].size()-1)
-				if (((*Matrix)[y][x + 1]) == C_track)
+				if (((*Matrix)[y][x + 1]) == C_track || (*Matrix)[y][x + 1] == C_food || (*Matrix)[y][x + 1] == C_drug)
 					direct = nextdir;
 			break;
 		case d_up:
-			if ((y == 0) && ((*Matrix)[(*Matrix).size() - 1][x] == C_track))
+			if ((y == 0) && ((*Matrix)[(*Matrix).size() - 1][x] == C_track || (*Matrix)[(*Matrix).size() - 1][x] == C_food))
 				y = (*Matrix).size() - 1;
 			if (y>0)
-				if (((*Matrix)[y - 1][x]) == C_track)
+				if (((*Matrix)[y - 1][x]) == C_track || (*Matrix)[y - 1][x] == C_food || (*Matrix)[y - 1][x] == C_drug)
 					direct = nextdir;
 			break;
 		case d_left:
-			if (x == 0 && ((*Matrix)[y][(*Matrix)[0].size() - 1] == C_track))
+			if (x == 0 && ((*Matrix)[y][(*Matrix)[0].size() - 1] == C_track || (*Matrix)[y][(*Matrix)[0].size() - 1] == C_food))
 				x = (*Matrix)[0].size() - 1;
 			if (x>0)
-				if (((*Matrix)[y][x - 1]) == C_track)
+				if (((*Matrix)[y][x - 1]) == C_track || (*Matrix)[y][x - 1] == C_food || (*Matrix)[y][x - 1] == C_drug)
 					direct = nextdir;
 			break;
 		case d_down:
-			if ((y == (*Matrix).size() - 1) && ((*Matrix)[0][x] == C_track))
+			if ((y == (*Matrix).size() - 1) && ((*Matrix)[0][x] == C_track || (*Matrix)[0][x] == C_food))
 				y = 0;
 			if (y<(*Matrix).size()-1)
-				if (((*Matrix)[y + 1][x]) == C_track)
+				if (((*Matrix)[y + 1][x]) == C_track || (*Matrix)[y + 1][x] == C_food || (*Matrix)[y + 1][x] == C_drug)
 					direct = nextdir;
 			break;
 		}
@@ -74,22 +79,22 @@ public:
 		{
 		case d_right: 
 			if(x<(*Matrix)[0].size()-1)
-			if (((*Matrix)[y][x + 1]) == C_track)
+			if (((*Matrix)[y][x + 1]) == C_track || ((*Matrix)[y][x + 1]) == C_food || ((*Matrix)[y][x + 1]) == C_drug)
 				x++;
 			break;
 		case d_up:
 			if (y>0)
-			if (((*Matrix)[y - 1][x]) == C_track)
+			if (((*Matrix)[y - 1][x]) == C_track || ((*Matrix)[y - 1][x]) == C_food || ((*Matrix)[y - 1][x]) == C_drug)
 				y--;
 			break;
 		case d_left:
 			if (x>0)
-			if (((*Matrix)[y][x - 1]) == C_track)
+			if (((*Matrix)[y][x - 1]) == C_track || ((*Matrix)[y][x - 1]) == C_food || ((*Matrix)[y][x - 1]) == C_drug)
 				x--;
 			break;
 		case d_down:
 			if (y<(*Matrix).size()-1)
-			if (((*Matrix)[y + 1][x]) == C_track)
+			if (((*Matrix)[y + 1][x]) == C_track || ((*Matrix)[y + 1][x]) == C_food || ((*Matrix)[y + 1][x]) == C_drug)
 				y++;
 			break;
 		default:
@@ -99,7 +104,7 @@ public:
 	void Draw(float w,float h,float wndh)
 	{
 		glBegin(GL_TRIANGLE_STRIP);
-		glColor3ub(255, 255, 0);
+		glColor3ub(0, 100, 0);
 		glVertex2f(w*x,wndh- h*y-h);
 		glVertex2f(w*x+w*4, wndh -h- h*y);
 		glVertex2f(w*x, wndh -h- h*y+h*4);
@@ -110,7 +115,10 @@ public:
 	{
 		nextdir = d;
 	}
-
+	bool Collision(const Man & m)
+	{
+		return abs(x - m.x) < 4 && abs(y - m.y)<4;
+	}
 protected:
 	std::vector<std::vector<Cubes>>* Matrix;
 	M_direct nextdir = d_left;
@@ -138,21 +146,20 @@ public:
 	}
 	void Step()
 	{
-
 		std::vector<M_direct>arr;
 
-		if ((*Matrix)[y + 1][x] == C_track && direct != d_up)
+		if (((*Matrix)[y + 1][x] == C_track || ((*Matrix)[y + 1][x]) == C_food || ((*Matrix)[y + 1][x]) == C_drug) && direct != d_up)
 			arr.push_back(d_down);
 
-		if ((*Matrix)[y - 1][x] == C_track && direct != d_down)
+		if (((*Matrix)[y - 1][x] == C_track || ((*Matrix)[y - 1][x]) == C_food || ((*Matrix)[y - 1][x]) == C_drug) && direct != d_down)
 			arr.push_back(d_up);
 
 		if (x < (*Matrix)[0].size() - 1)
-			if ((*Matrix)[y][x + 1] == C_track && direct != d_left)
+			if (((*Matrix)[y][x + 1] == C_track || ((*Matrix)[y][x + 1]) == C_food || ((*Matrix)[y][x + 1]) == C_drug)&& direct != d_left)
 				arr.push_back(d_right);
 
 		if (x > 0)
-			if ((*Matrix)[y][x - 1] == C_track && direct != d_right)
+			if (((*Matrix)[y][x - 1] == C_track || ((*Matrix)[y][x - 1]) == C_food || ((*Matrix)[y][x - 1]) == C_drug) && direct != d_right)
 				arr.push_back(d_left);
 
 		if (arr.empty())
@@ -165,4 +172,38 @@ public:
 	
 private:
 	void SetDirect(M_direct d) {}
+};
+
+class Pac :public Man
+{
+public:
+	Pac (std::vector<std::vector<Cubes>>* matrix):Man(matrix){}
+	~Pac() {}
+	
+	void Draw(float w, float h, float wndh)
+	{
+		glBegin(GL_TRIANGLE_STRIP);
+		if(undyingTimer == 0)
+			glColor3ub(255, 255, 0);
+		else glColor3ub(0, 0, 255);
+		glVertex2f(w*x, wndh - h*y - h);
+		glVertex2f(w*x + w * 4, wndh - h - h*y);
+		glVertex2f(w*x, wndh - h - h*y + h * 4);
+		glVertex2f(w*x + w * 4, wndh - h - h*y + h * 4);
+		glEnd();
+	}
+
+	void Step()
+	{
+		Man::Step();
+		if ((*Matrix)[y][x] == C_drug) 
+			undyingTimer = 70;
+		
+		if (undyingTimer > 0)
+			undyingTimer--;
+
+		(*Matrix)[y][x] = C_track;
+	}
+	int undyingTimer = 0;
+private:
 };
--- PackMan/main.cpp
+++ PackMan/main.cpp
@@ -5,10 +5,29 @@ using namespace std;
 float WndW = 400.f, WndH = 400.f;
 vector<vector<Cubes>> Matrix;
 vector<Spook> Spooks(4, &Matrix);
-Man packman(&Matrix);
+Pac packman(&Matrix);
+Game_stat Game = G_game;
 
 void Init();
-void open();
+void Open();
+void NewGame();
+
+void CollisionDetection()
+{
+	for (size_t i = 0; i < Spooks.size(); i++) 
+	{
+		if (packman.Collision(Spooks[i])) 
+		{
+			if (packman.undyingTimer == 0) 
+			{
+				Game = G_over;
+			}else {
+				Spooks[i].x = 29;
+				Spooks[i].y = 25;
+			}
+		}
+	}
+}
 
 void Draw(float x,float  y,float w,float h, Cubes b)
 {
@@ -20,6 +39,10 @@ void Draw(float x,float  y,float w,float h, Cubes b)
 	case C_track:glColor3ub(0, 150,200);
 		break;
 	case C_void:glColor3ub(0, 180, 200);
+		break;
+	case C_food:glColor3ub(100, 220, 200);
+		break;
+	case C_drug:glColor3ub(255, 255, 0);
 		break;
 	}
 	glVertex2f(x, y);
@@ -52,7 +75,14 @@ void Display()
 	glutSwapBuffers();
 }
 
-void Keys(int key,int ax,int ay)
+void Keys(unsigned char key, int ax, int ay)
+{
+	if (key == 'r')
+	{
+		NewGame();
+	}
+}
+void Keys(int key, int ax, int ay)
 {
 	switch (key)
 	{// d_up, d_left, d_down, d_right
@@ -65,11 +95,14 @@ void Keys(int key,int ax,int ay)
 
 void NextStep(int t = 0)
 {
+	if (Game != G_game)
+		return;
 	glutPostRedisplay();
 	glutTimerFunc(100, NextStep, 0);
 	for (size_t i = 0; i < Spooks.size(); i++)
 		Spooks[i].Step();
 	packman.Step();
+	CollisionDetection();
 }
 
 int main(int argc ,char** argv)
@@ -87,6 +120,7 @@ int main(int argc ,char** argv)
 	Init();
 
 	glutSpecialFunc(Keys);
+	glutKeyboardFunc(Keys);
 	glutDisplayFunc(Display);
 	glutMainLoop();
 	return 0;
@@ -95,13 +129,13 @@ int main(int argc ,char** argv)
 void Init()
 {
 	srand(time(NULL));
-	open();
-	NextStep();
+	NewGame();
 }
 
-void open()
+void Open()
 {
 	ifstream fin("square.txt");
+	Matrix.clear();
 	if (fin.is_open())
 	{
 		char t;
@@ -117,4 +151,28 @@ void open()
 			}
 		}
 	}
+}
+
+void NewGame()
+{
+	Open();
+	for (size_t i = 0; i < Matrix.size(); i++)
+	{
+		for (size_t j = 0; j < Matrix[i].size(); j++)
+		{
+			if (Matrix[i][j] == C_track)
+				Matrix[i][j] = C_food;
+		}
+	}
+	for (size_t i = 0; i < Spooks.size(); i++)
+	{
+		Spooks[i].x = 29;
+		Spooks[i].y = 25;
+	}
+	
+	packman.x = 3;
+	packman.y = 5;
+	packman.undyingTimer = 0;
+	Game = G_game;
+	NextStep();
 }
